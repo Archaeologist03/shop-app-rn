e0a4478ab002bc0b242f4d3d65ec63f790214b27
Archaeologist03 <omniiknight@hotmail.com>
2020-07-13 23:51:52 +0200

    Products Overview screen (initial structure) - 4

---
+++ data/dummy-data.js
@@ -0,0 +1,32 @@
+import Product from '../models/product';
+
+const PRODUCTS = [
+  new Product(
+    'p1',
+    'u1',
+    'Red Shirt',
+    'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    'A red t-shirt, perfect for days with non-red weather.',
+    29.99
+  ),
+  new Product(
+    'p2',
+    'u1',
+    'Blue Carpet',
+    'https://images.pexels.com/photos/6292/blue-pattern-texture-macro.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    'Fits your red shirt perfectly. To stand on. Not to wear it.',
+    99.99
+  ),
+  new Product(
+    'p3',
+    'u2',
+    'Coffee Mug',
+    'https://images.pexels.com/photos/160834/coffee-cup-and-saucer-black-coffee-loose-coffee-beans-160834.jpeg?cs=srgb&dl=bean-beans-black-coffee-160834.jpg&fm=jpg',
+    'Can also be used for tea!',
+    8.99
+  ),
+  new Product(
+    'p4',
+    'u3',
+    'The Book - Limited Edition',
+    'https://images.pexels.com/photos/46274/pexels-photo-46
